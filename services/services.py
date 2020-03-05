d56ba0806c12921489e9fe8a12d5eb00d9a4c20f
Prabhudayal Acharya <prabhudayala@gmail.com>
2020-03-05 07:39:04 +0530

    added tensorflow saved model and object detection process for image

--- services/services.py
+++ services/services.py
@@ -3,16 +3,85 @@ from werkzeug.utils import secure_filename
 import imutils
 import cv2
 import base64
+import os
+import cv2
+import numpy as np
+import tensorflow as tf
+import sys
+
+from utils import label_map_util
+from utils import visualization_utils as vis_util
 
 
 def process_image(reqData, upload_folder):
     try:
+        CWD_PATH = os.getcwd()
+
+        PATH_TO_CKPT = os.path.join(CWD_PATH,'services', 'frozen_inference_graph.pb')
+
+        PATH_TO_LABELS = os.path.join(CWD_PATH,'services', 'label_map.pbtxt')
+
+
+        NUM_CLASSES = 15
+
+
+        label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+        categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+        category_index = label_map_util.create_category_index(categories)
+
+        detection_graph = tf.Graph()
+        with detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+            sess = tf.Session(graph=detection_graph)
+
+        # Define input and output tensors (i.e. data) for the object detection classifier
+
+        # Input tensor is the image
+        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+        # Output tensors are the detection boxes, scores, and classes
+        # Each box represents a part of the image where a particular object was detected
+        detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+        # Each score represents level of confidence for each of the objects.
+        # The score is shown on the result image, together with the class label.
+        detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+
+        # Number of objects detected
+        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+        
         f = reqData.files['file']
         file_save_path = os.path.join(
             upload_folder, "images", secure_filename(f.filename))
         f.save(file_save_path)
 
         image = cv2.imread(os.path.join(file_save_path))
+
+        image_expanded = np.expand_dims(image, axis=0)
+
+        # Perform the actual detection by running the model with the image as input
+        (boxes, scores, classes, num) = sess.run(
+            [detection_boxes, detection_scores, detection_classes, num_detections],
+            feed_dict={image_tensor: image_expanded})
+
+        # Draw the results of the detection (aka 'visulaize the results')
+
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            image,
+            np.squeeze(boxes),
+            np.squeeze(classes).astype(np.int32),
+            np.squeeze(scores),
+            category_index,
+            use_normalized_coordinates=True,
+            line_thickness=8,
+            min_score_thresh=0.60)
+
         _, img_encoded = cv2.imencode(".jpg", image)
         imag64 = base64.b64encode(img_encoded)
         # print(imag64)
